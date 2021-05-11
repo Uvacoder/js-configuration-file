ae3b8b482c9538f9e782088c6699dd9c055b61c8
Giuseppe Gigliotti <giuseppegigliottidev@gmail.com>
2021-05-11 22:18:11 +0200

    better performance

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,29 +2,21 @@ const fs = require('fs');
 const path = require('path');
 
 const prettierOptions = JSON.parse(
-    fs.readFileSync(path.resolve(__dirname, '.prettierrc'), 'utf8'),
- );
+  fs.readFileSync(path.resolve(__dirname, '.prettierrc'), 'utf8'),
+);
 
 module.exports = {
-    "env": {
-        "browser": true,
-        "node": true,
-        "commonjs": true,
-        "es2021": true
-    },
-    "extends": ["eslint:recommended", "airbnb", "prettier", "plugin:node/recommended"],
-    "parserOptions": {
-        "ecmaVersion": 12,
-        "sourceType": "module"
-    },
-    "plugins": ["prettier"],
-    "rules": {
-        'prettier/prettier': ['error', prettierOptions],
-        "no-unusued-vars":"warn",
-        "no-console":"off",
-        "func-names":"off",
-        "no-process-exit": "warn",
-        "object-shorthand": "off",
-        "class-methods-use-this":"off"
-    }
-}
+  env: {
+    browser: true,
+    jest: true,
+  },
+  extends: ['airbnb-react', 'react-app', 'prettier', 'prettier/react'],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': ['error', prettierOptions],
+  },
+};
